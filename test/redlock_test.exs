9a080b653090b4b93e1f03ec97a4733da9f4a4fa
lyokato <lyo.kato@gmail.com>
2018-09-06 03:22:16 +0000

    add tests

--- test/redlock_test.exs
+++ test/redlock_test.exs
@@ -1,5 +1,55 @@
 defmodule RedlockTest do
+
   use ExUnit.Case
-  doctest Redlock
+
+  test "transaction" do
+    key = "foo"
+    result = Redlock.transaction(key, 5, fn ->
+      {:ok, 2}
+    end)
+
+    assert result == {:ok, 2}
+  end
+
+  test "confliction" do
+    key = "same"
+
+    # lock with enough duration
+    {:ok, mutex} = Redlock.lock(key, 20)
+
+    # try to lock, and do some retry internally
+    result1 = Redlock.transaction(key, 5, fn ->
+      {:ok, 2}
+    end)
+
+    # should fail eventually
+    assert result1 == {:error, :lock_failure}
+
+    assert Redlock.unlock(key, mutex) == :ok
+
+    result2 = Redlock.transaction(key, 1, fn ->
+      {:ok, 2}
+    end)
+
+    assert result2 == {:ok, 2}
+
+  end
+
+  test "expiration" do
+
+    key = "bar"
+
+    # this lock automatically expired in 1 seconds.
+    {:ok, mutex} = Redlock.lock(key, 1)
+
+    # try to lock, and do some retry internally.
+    # the retry-configuration has enough interval.
+    result1 = Redlock.transaction(key, 5, fn ->
+      {:ok, 2}
+    end)
+
+    # should success eventually
+    assert result1 == {:ok, 2}
+  end
 
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,20 @@
+# before execute this tests, you sould start redis server on your host.
+
+redlock_conf = [
+  pool_size: 2,
+  max_retry: 5,
+  retry_interval_base: 300,
+  retry_interval_max: 3000,
+  reconnection_interval_base: 300,
+  reconnection_interval_max: 3000,
+  servers: [
+    [host: "127.0.0.1", port: 6379],
+  ]
+]
+
+Supervisor.start_link(
+  [{Redlock, redlock_conf}],
+  [strategy: :one_for_one, name: Redlock.TestSupervisor]
+)
+
 ExUnit.start()
